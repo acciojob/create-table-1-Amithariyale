c26a738a5a07194b84db5a75a9e1343eaff5a465
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2023-12-23 00:15:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	const table=document.querySelector('#sampleTable');
+	const tr=document.createElement('tr');
+	const colOne=document.createElement('td');
+	const colTwo=document.createElement('td');
+
+	colOne.innerText="New Cell1"
+	colTwo.innerText="New Cell2"
+	tr.append(colOne,colTwo);
+	table.append(tr);
+	  console.log(colOne)
 }
